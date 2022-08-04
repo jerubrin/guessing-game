e19cf80d5306035027bf98adec878fc1e944f197
Alexey Kuptsov <60064512+jerubrin@users.noreply.github.com>
2022-08-04 21:22:37 +0600

    feat: all done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.gNum = Math.round ((this.max - this.min) / 2 + this.min)
+        return this.gNum
     }
 
     lower() {
-
+        this.max = this.gNum
     }
 
     greater() {
-
+        this.min = this.gNum
     }
 }
 
